c3999d6f9859dfcca20f0e5c224ee3d1ddb72a07
Literally Collective <bluedragon.cstar@outlook.com>
2022-01-27 06:06:22 -0300

    Feat: publish uikit

---
+++ src/widgets/Menu/components/CakePrice.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import { PancakeRoundIcon } from "../../../components/Svg";
+import Text from "../../../components/Text/Text";
+import Skeleton from "../../../components/Skeleton/Skeleton";
+import { Link } from "../../../components/Link";
+
+interface Props {
+  cakePriceUsd?: number;
+}
+
+const PriceLink = styled.span`
+  display: flex;
+  align-items: center;
+
+  svg {
+    transition: transform 0.3s;
+  }
+`;
+
+const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
+  return cakePriceUsd ? (
+    <PriceLink>
+      <PancakeRoundIcon width="24px" mr="8px" />
+      <Link
+        href={"https://ftmscan.com/token/0x7Ad70C360f28A445286D37214Bbd72D9dc492B9b"}
+        style={{ marginLeft: "5px", textDecoration: "none" }}
+        target="_blank"
+        color="textSubtle"
+        bold
+      >{`$${cakePriceUsd.toFixed(3)}`}</Link>
+    </PriceLink>
+  ) : (
+    <Skeleton width={80} height={24} />
+  );
+};
+
+export default React.memo(CakePrice);
